237718c100e4e6db832e6eeb71aebffb6605ae4c
shubhamsrnm <shubhamsrmn@gmail.com>
2023-04-14 19:12:22 +0530

    fix fetching bug

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,7 +1,7 @@
 export const fetchFun = async function (location) {
   try {
     const res = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=4ee96dfd14d44dfcb72141708231204&q=${location}&aqi=yes`
+      `https://api.weatherapi.com/v1/current.json?key=4ee96dfd14d44dfcb72141708231204&q=${location}&aqi=yes`
     );
     if (res.status !== 200) throw new Error("error");
     const data = await res.json();
@@ -14,7 +14,7 @@ export const fetchFun = async function (location) {
 };
 export async function getLocationNFetch() {
   const res = await fetch(
-    "http://api.weatherapi.com/v1/ip.json?key=4ee96dfd14d44dfcb72141708231204&q=auto:ip"
+    "https://api.weatherapi.com/v1/ip.json?key=4ee96dfd14d44dfcb72141708231204&q=auto:ip"
   );
 
   const data = await res.json();
